bab06b601a3cb71e308df9c8cb715ae298bc8f04
jijunyu <jijunyu@fangjia.com>
2018-02-05 16:42:09 +0800

    baise

---
+++ crawler/baise_1.py
@@ -0,0 +1,24 @@
+"""
+url = http://www.bsfcj.com/PubInfo/HouseSource.asp?page=1&xm_xzqy=&xm_xmmc=&xm_xmdz=&xm_kfs=&xm_fwhx=
+city: 百色
+num: 1
+"""
+
+import requests
+from lxml import etree
+
+class Baise:
+    url = 'http://www.bsfcj.com/PubInfo/HouseSource.asp?page=1&xm_xzqy=&xm_xmmc=&xm_xmdz=&xm_kfs=&xm_fwhx='
+
+    def baise_start(self):
+        requests.get(url=self.url)
+
+    def get_all_page(self):
+        res = requests.get(url=self.url)
+        html = etree.HTML(res.content.decode('gb2312'))
+        html.xpath('')
+
+
+if __name__ == '__main__':
+    b = Baise()
+    b.get_all_page()
