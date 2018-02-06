4a31b7d5e8f0dcf41a5d8e0030e74ffe389cc118
jijunyu <jijunyu@fangjia.com>
2018-02-06 10:46:01 +0800

    building

--- comm_info.py
+++ comm_info.py
@@ -1,8 +1,25 @@
 """
-小区字段
+Comm :小区字段
+Building:楼栋和房号字段
+House:房屋类型
 """
 from lib.mongo import Mongo
 import datetime
+
+
+def serialization_info(info):
+    """
+
+    :param comm_info:
+    :return: data:
+    """
+    data = {}
+    for key, value in vars(info).items():
+        if key is 'coll':
+            continue
+        data[key] = value
+    return data
+
 
 class Comm:
     def __init__(self, co_index=None, co_name=None, co_id=None, co_address=None, co_type=None, co_green=None,
@@ -25,19 +42,47 @@ class Comm:
         self.co_volumetric = co_volumetric  # 容积率
 
         self.time = datetime.datetime.now()
-        self.coll = Mongo('192.168.0.235', 27017, 'gv', 'gv').get_collection_object()
+        self.coll = Mongo('192.168.0.235', 27017, 'gv', 'community').get_collection_object()
 
-    def comm_to_mongo(self):
-        data = {}
-        for key, value in vars(self).items():
-            if key is 'coll':
-                continue
-            data[key] = value
+    def insert_db(self):
+        data = serialization_info(self)
+        print(data)
         self.coll.insert_one(data)
+
+
+class Building:
+    def __init__(self, co_index=None, co_id=None, bu_num=None, bu_all_house=None, bu_floor=None, bu_build_size=None,
+                 bu_live_size=None, bu_not_live_size=None, bu_price=None):
+        self.co_index = co_index  # 网站id
+        self.co_id = co_id  # 小区id
+
+        self.bu_num = bu_num  # 楼号 栋号
+        self.bu_all_house = bu_all_house  # 总套数
+        self.bu_floor = bu_floor  # 楼层
+
+        self.bu_build_size = bu_build_size  # 建筑面积
+        self.bu_live_size = bu_live_size  # 住宅面积
+        self.bu_not_live_size = bu_not_live_size  # 非住宅面积
+
+        self.bu_price = bu_price  # 住宅价格
+
+        self.time = datetime.datetime.now()
+        self.coll = Mongo('192.168.0.235', 27017, 'gv', 'building').get_collection_object()
+
+    def insert_db(self):
+        data = serialization_info(self)
+        print(data)
+        self.coll.insert_one(data)
+
+
+class House:
+    def __init__(self):
+        self.time = datetime.datetime.now()
+        self.coll = Mongo('192.168.0.235', 27017, 'gv', 'house').get_collection_object()
 
 
 if __name__ == '__main__':
     c = Comm()
     c.co_index = '1100'
     c.co_name = 'mingzi'
-    c.comm_to_mongo()
+    c.insert_db()
