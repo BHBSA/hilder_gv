abe9726ad79d4a3f6a751431f479e6a7f1d9a2e4
lvsanli <lvsanli@fangjia.com>
2018-02-24 10:05:31 +0800

    池州

--- crawler/chizhou_6.py
+++ crawler/chizhou_6.py
@@ -94,89 +94,80 @@ class Chizhou(Crawler):
 
     @retry(retry(3))
     def get_build_info(self, build_url, co_id):
-        try:
-            response = requests.get(build_url)
-            html = response.content.decode('gbk')
-            tree = etree.HTML(html)
-            bu_detail_url = tree.xpath('/html/body/table/tr/td/table/tr/td/table[1]/tr/td[1]/input/@onclick')
-            for i in bu_detail_url:
-                building = Building()
-                bu_detail_url = re.search("location='(.*?)'", i).group(1)
-                bu_detail_url = 'http://www.czfdc.gov.cn/spf/' + bu_detail_url
-                building = self.get_build_detail_info(bu_detail_url, building, co_id)
-                building.co_id = co_id
-                building.co_index = CO_INDEX
-                # 插入数据库
-                building.insert_db()
-        except requests.exceptions.ConnectTimeout:
-            print('build_url:', build_url)
+        response = requests.get(build_url)
+        html = response.content.decode('gbk')
+        tree = etree.HTML(html)
+        bu_detail_url = tree.xpath('/html/body/table/tr/td/table/tr/td/table[1]/tr/td[1]/input/@onclick')
+        for i in bu_detail_url:
+            building = Building()
+            bu_detail_url = re.search("location='(.*?)'", i).group(1)
+            bu_detail_url = 'http://www.czfdc.gov.cn/spf/' + bu_detail_url
+            building = self.get_build_detail_info(bu_detail_url, building, co_id)
+            building.co_id = co_id
+            building.co_index = CO_INDEX
+            # 插入数据库
+            building.insert_db()
 
     @retry(retry(3))
     def get_build_detail_info(self, bu_detail_url, building, co_id):
-        try:
-            response = requests.get(bu_detail_url)
-            html = response.content.decode('gbk')
-            tree = etree.HTML(html)
-            # 楼层
-            bu_floor = tree.xpath('/html/body/table[4]/tr/td/table[2]/tr[2]/td[1]/text()')
-            bu_floor = self.is_none(bu_floor).strip()
-            # 总套数
-            bu_all_house = tree.xpath('/html/body/table[2]/tr/td[1]/table[1]/tr[3]/td[1]/text()')
-            bu_all_house = self.is_none(bu_all_house).replace('套(间)', '')
-            # 楼栋名称
-            bu_name = tree.xpath('/html/body/table[3]/tr/*/*/*/*/option[@selected]/text()')
-            bu_name = self.is_none(bu_name)
-            # 楼栋id
-            bu_id = re.search('buildid=(\d+?)&', bu_detail_url).group(1)
-            building.bu_floor = bu_floor
-            building.co_index = CO_INDEX
-            building.bu_all_house = bu_all_house
-            building.bu_name = bu_name
-            building.bu_id = bu_id
-            house_url_list = tree.xpath('/html/body/table[4]/tr/td/table[2]/tr/td[not(@bgcolor="#ffffff")]/a/@href')
-            for i in house_url_list:
-                house = House()
-                house_url = 'http://www.czfdc.gov.cn/spf/' + i
-                self.get_house_info(house_url, house, co_id, bu_id)
-            return building
-        except requests.exceptions.ConnectTimeout:
-            print('bu_detail_url:', bu_detail_url)
+        response = requests.get(bu_detail_url)
+        html = response.content.decode('gbk')
+        tree = etree.HTML(html)
+        # 楼层
+        bu_floor = tree.xpath('/html/body/table[4]/tr/td/table[2]/tr[2]/td[1]/text()')
+        bu_floor = self.is_none(bu_floor).strip()
+        # 总套数
+        bu_all_house = tree.xpath('/html/body/table[2]/tr/td[1]/table[1]/tr[3]/td[1]/text()')
+        bu_all_house = self.is_none(bu_all_house).replace('套(间)', '')
+        # 楼栋名称
+        bu_name = tree.xpath('/html/body/table[3]/tr/*/*/*/*/option[@selected]/text()')
+        bu_name = self.is_none(bu_name)
+        # 楼栋id
+        bu_id = re.search('buildid=(\d+?)&', bu_detail_url).group(1)
+        building.bu_floor = bu_floor
+        building.co_index = CO_INDEX
+        building.bu_all_house = bu_all_house
+        building.bu_name = bu_name
+        building.bu_id = bu_id
+        house_url_list = tree.xpath('/html/body/table[4]/tr/td/table[2]/tr/td[not(@bgcolor="#ffffff")]/a/@href')
+        for i in house_url_list:
+            house = House()
+            house_url = 'http://www.czfdc.gov.cn/spf/' + i
+            self.get_house_info(house_url, house, co_id, bu_id)
+        return building
 
     @retry(retry(3))
     def get_house_info(self, house_url, house, co_id, bu_id):
-        try:
-            response = requests.get(house_url)
-            html = response.content.decode('gbk')
-            tree = etree.HTML(html)
-            ho_num = tree.xpath('/html/body/table/tr[2]/td[4]/text()')
-            if not ho_num:
-                return
-            ho_num = self.is_none(ho_num)
-            ho_floor = tree.xpath('/html/body/table/tr[2]/td[2]/text()')
-            ho_floor = self.is_none(ho_floor).replace('层', '')
-            ho_price = tree.xpath('/html/body/table/tr[6]/td[2]/text()')
-            ho_price = self.is_none(ho_price).replace('元/m','')
-            ho_build_size = tree.xpath('/html/body/table/tr[4]/td[2]/text()')
-            ho_build_size = self.is_none(ho_build_size).replace('m', '')
-            ho_true_size = tree.xpath('/html/body/table/tr[4]/td[4]/text()')
-            ho_true_size = self.is_none(ho_true_size).replace('m', '')
-            ho_share_size = tree.xpath('/html/body/table/tr[5]/td[2]/text()')
-            ho_share_size = self.is_none(ho_share_size).replace('m', '')
-            ho_room_type = tree.xpath('/html/body/table/tr[3]/td[4]/text()')
-            ho_room_type = self.is_none(ho_room_type)
-            house.co_index = CO_INDEX
-            house.co_id = co_id
-            house.bu_id = bu_id
-            house.ho_num = ho_num
-            house.ho_floor = ho_floor
-            house.ho_price = ho_price
-            house.ho_build_size = ho_build_size
-            house.ho_true_size = ho_true_size
-            house.ho_share_size = ho_share_size
-            house.ho_room_type = ho_room_type
-            house.insert_db()
-        except requests.exceptions.ConnectTimeout:
-            print('house_url:', house_url)
+        response = requests.get(house_url)
+        html = response.content.decode('gbk')
+        tree = etree.HTML(html)
+        ho_num = tree.xpath('/html/body/table/tr[2]/td[4]/text()')
+        if not ho_num:
+            return
+        ho_num = self.is_none(ho_num)
+        ho_floor = tree.xpath('/html/body/table/tr[2]/td[2]/text()')
+        ho_floor = self.is_none(ho_floor).replace('层', '')
+        ho_price = tree.xpath('/html/body/table/tr[6]/td[2]/text()')
+        ho_price = self.is_none(ho_price)
+        ho_build_size = tree.xpath('/html/body/table/tr[4]/td[2]/text()')
+        ho_build_size = self.is_none(ho_build_size).replace('m', '')
+        ho_true_size = tree.xpath('/html/body/table/tr[4]/td[4]/text()')
+        ho_true_size = self.is_none(ho_true_size).replace('m', '')
+        ho_share_size = tree.xpath('/html/body/table/tr[5]/td[2]/text()')
+        ho_share_size = self.is_none(ho_share_size).replace('m', '')
+        ho_room_type = tree.xpath('/html/body/table/tr[3]/td[4]/text()')
+        ho_room_type = self.is_none(ho_room_type)
+        house.co_index = CO_INDEX
+        house.co_id = co_id
+        house.bu_id = bu_id
+        house.ho_num = ho_num
+        house.ho_floor = ho_floor
+        house.ho_price = ho_price
+        house.ho_build_size = ho_build_size
+        house.ho_true_size = ho_true_size
+        house.ho_share_size = ho_share_size
+        house.ho_room_type = ho_room_type
+        house.insert_db()
 
 
 if __name__ == '__main__':
