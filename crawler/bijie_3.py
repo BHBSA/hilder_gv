eacafa83c0919b5397d596ebe7cc3dc7a5372538
周彤云 <zhoutongyun@fangjia.com>
2018-03-02 14:02:49 +0800

    毕节

--- crawler/bijie_3.py
+++ crawler/bijie_3.py
@@ -2,55 +2,137 @@
 url:http://www.gzbjfc.com/House.aspx
 city: 毕节
 CO_INDEX: 3
-author : j
+author : 周彤云
+小区数：660
+time:2018-02-11
 """
+CO_INDEX = 3
 import requests
 from crawler_base import Crawler
 from comm_info import Comm, Building, House
 from retry import retry
 from lxml import etree
 
-CO_INDEX = 3
-
-
 class Bijie(Crawler):
     def __init__(self):
         self.start_url = 'http://www.gzbjfc.com/House.aspx'
 
-    @retry(retry(3))
     def start_crawler(self):
-        res = requests.get(url=self.start_url)
+        self.bijie_start()
 
-        # 获取所有分页的url
-        url_list = self.get_all_comm_list_url(res.content.decode())
-        self.crawler_comm_url(url_list)
+    @retry(retry(3))
+    def get_all_page(self):
+        try:
+            res = requests.get(url=self.start_url)
+            html = res.content.decode()
+            # print(html)
+            tree = etree.HTML(html)
+            #获取总页数
+            page =tree.xpath('//div[@id="cph_hl1_pager"]/a[12]/@href')[0].split('=')[1]
+            return page
+        except Exception as e:
+            print('retry')
+            raise
 
-    # @retry(retry(3))
-    def crawler_comm_url(self, url_list):
-        # 请求所有的小区列表页面
-        for i in url_list:
-            c = Comm()
-            res = requests.get(i)
-            html = etree.HTML(res.content.decode())
-            c.co_name = html.xpath(
-                '//*[@id="form1"]/div/div[2]/div[2]/div[3]/div[2]/table[1]/tbody/tr/td/table/tbody/tr[1]/td[2]/div/a')
-            print(c.co_name)
-            break
+    @retry(retry(3))
+    def bijie_start(self):
+        try:
+            page = self.get_all_page()
+            for i in range(1, int(page)+1):
+                # print(i)
+                res = requests.get(self.start_url+'?page=' + str(i))
+                html = res.content
+                tree = etree.HTML(html)
+                # print(res)
+                #获取每一页小区列表的url
+                comm_list =tree.xpath('//table[@class="Repeater"]//div[@align="left"]/a[@class="url"]/@href')
+                # print(comm_list)
+                for i in comm_list:
+                    comm = Comm()
+                    comm_url = 'http://www.gzbjfc.com' + i
+                    # print(comm_url)
+                    comm_obj = self.get_comm_detail(comm_url)
+                    # comm_obj.insert_db()
+        except Exception as e:
+            print('retry')
+            raise
 
 
-@staticmethod
-def get_all_comm_list_url(page):
-    """
+    @retry(retry(3))
+    def get_comm_detail(self, comm_url):
+        try:
+             res = requests.get(url=comm_url)
+             co_id = res.url
+             print(co_id)
+             html = res.content.decode()
+             tree = etree.HTML(html)
+             # 获取小区页面字段
+             co_name = tree.xpath('//span[@id="cph_hif1_xmmc"]/font/text()')[0]  # 小区名称
+             # print(co_name)
+             co_pre_sale = tree.xpath('//span[@id="cph_hif1_xsxkz"]/font/text()')[0]  # 预售证书
+             # print(co_pre_sale)
+             co_address = tree.xpath('//span[@id="cph_hif1_zl"]/font/text()')[0]  # 小区地址
+             # print(co_address)
+             co_develops = tree.xpath('//span[@id="cph_hif1_kfs"]/font/text()')[0]  # 开发商
+             # print(co_develops)
+             co_build_size = tree.xpath('//span[@id="cph_hif1_jzmj"]/text()')[0]  # 建筑面积
+             # print(co_build_size)
+             co_green = tree.xpath('//span[@id="cph_hif1_lhl"]/text()')[0]  # 绿化率
+             if not co_green:
+                 return
+             # print(co_green)
+             co_volumetric = tree.xpath('//span[@id="cph_hif1_rjl"]/text()')[0]  # 容积率
+             # print(co_volumetric)
+             co_all_house = tree.xpath('//span[@id="cph_hif1_fwts"]/text()')[0]  # 小区总套数
+             # print(co_all_house)
+             co_open_time = tree.xpath('//span[@id="cph_hif1_kpsj"]/font/text()')[0]  # 小区开盘时间
+             # print(co_open_time)
+             co_handed_time = tree.xpath('//span[@id="cph_hif1_jfsj"]/font/text()')[0]  # 小区交房时间
+             # print(co_handed_time)
+             # comm.co_name = co_name
+             # comm.co_pre_sale = co_pre_sale
+             # comm.co_address = co_address
+             # comm.co_develops = co_develops
+             # comm.co_build_size = co_build_size
+             # comm.co_green = co_green
+             # comm.co_volumetric = co_volumetric
+             # comm.co_all_house = co_all_house
+             # comm.co_open_time = co_open_time
+             # comm.co_handed_time = co_handed_time
+             # 获取楼栋url
+             build_url = tree.xpath('//a[@id="hdl1_hlink3"]/@href')[0]
+             building = Building()
+             build_obj = self.get_build_detail(build_url, building)
+             # build_obj.insert_db()
+        except Exception as e:
+            print('retry')
+            raise
 
-    :param page: 首页html
-    :return: url list
-    """
-    comm_list = []
-    html = etree.HTML(page)
-    page_num = int(html.xpath('//*[@id="cph_hl1_pagerTop"]/a[12]')[0].attrib['href'].split('page=')[1])
-    print(page_num)
-    for i in range(1, page_num + 1):
-        comm_list.append('http://www.gzbjfc.com/House.aspx?page=' + str(i))
 
-    print(comm_list)
-    return comm_list
+    @retry(retry(3))
+    def get_build_detail(self, build_url):
+        res = requests.get(url=build_url)
+        html = res.content.decode()
+        tree = etree.HTML(html)
+        # 获取楼栋信息
+        build_content = tree.xpath('//table[@id="cph_hb1_dg1"]/tbody/tr[position()>1]')
+        for i in build_content:
+            bu_num = build_content.xpath('td[1]/text')[0]
+            bu_all_house = build_content.xpath('td[3]/text')[0]
+            building.bu_num = bu_num
+            building.bu_all_house = bu_all_house
+            # building_data =
+            ho_url = build_content.xpath('td[7]/font/a/@href')
+            house = House()
+            house_obj = self.get_house_detail(ho_url)
+
+
+    # def get_house_d etail(self, ho_url):
+
+
+
+
+if __name__ == '__main__':
+    b = Bijie()
+    b.start_crawler()
+
