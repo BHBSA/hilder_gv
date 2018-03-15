4614c48c8956ed59eb094aae185fe48f953a5129
lvsanli <lvsanli@fangjia.com>
2018-03-15 14:49:59 +0800

    广安 白色

--- new_crawler/guangan_12.py
+++ new_crawler/guangan_12.py
@@ -3,124 +3,115 @@ url = http://www.gafdc.cn/newhouse/houselist.aspx?hou=0-0-0-0-0-0-&page=1
 city : 广安
 CO_INDEX : 12
 author: 吕三利
-小区数量：14页    2018/3/1
+小区数量：14页    2018/3/1\
+楼栋数据加载不出来
 """
 from crawler_base import Crawler
-from lxml import etree
 from comm_info import Comm, Building, House
-import requests, re
-from producer import ProducerListUrl, do_request
 from get_page_num import AllListUrl
+from producer import ProducerListUrl
+import requests, re
+
+url = 'http://www.gafdc.cn/newhouse/houselist.aspx?hou=0-0-0-0-0-0-&page=1'
+co_index = '12'
+city = '广安'
 
 
 class Guangan(Crawler):
     def __init__(self):
-        self.url = 'http://www.gafdc.cn/newhouse/houselist.aspx?hou=0-0-0-0-0-0-&page=1'
+        self.headers = {
+            'User-Agent':
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119Safari/537.36'
+        }
 
     def start_crawler(self):
-        self.start()
-
-    def start(self):
-        b = AllListUrl(first_page_url=self.url,
+        b = AllListUrl(first_page_url=url,
                        request_method='get',
                        analyzer_type='regex',
                        encode='utf-8',
                        page_count_rule='pg.pageCount = (.*?);',
                        )
         page = b.get_page_count()
+        all_url_list = []
         for i in range(1, int(page) + 1):
             all_url = 'http://www.gafdc.cn/newhouse/houselist.aspx?hou=0-0-0-0-0-0-&page=' + str(i)
-            response = requests.get(all_url)
+            comm_url_list = self.get_comm_url(all_url)
+            all_url_list += comm_url_list
+        # 遍历所有小区url
+        for i in all_url_list:
+            comm_url = 'http://www.gafdc.cn/newhouse/' + str(i.replace('index', 'base'))
+            self.get_comm_info(comm_url)
+        all_build_url_list = []
+        for i in all_url_list:
+            build_url = 'http://www.gafdc.cn/newhouse/' + str(i.replace('index', 'table'))
+            house_url_list = self.get_build_info(build_url)
+            all_build_url_list += house_url_list
+        all_house_url_list = []
+        form_data_list = []
+        for i in all_build_url_list:
+            house_url = 'http://www.gafdc.cn/newhouse/GetBuildTableByAjax.ashx'
+            data = {
+                'itemRecord': i[0],
+                'houseCode': i[1]
+            }
+            all_house_url_list.append(house_url)
+            form_data_list.append(data)
+        self.get_house_info(form_data_list)
+
+    def get_house_info(self, form_data_list):
+        for data in form_data_list:
+            url = 'http://www.gafdc.cn/newhouse/GetBuildTableByAjax.ashx'
+            response = requests.post(url=url, data=data)
             html = response.text
-            tree = etree.HTML(html)
-            comm_detail_list = tree.xpath('//div[@class="newexleft"]/div/ul[2]/li[1]/a/@href')
-            for i in comm_detail_list:
-                c = Comm(12)
-                i = i.replace('index', 'base')
-                comm_url = 'http://www.gafdc.cn/newhouse/' + i
-                c.co_id = re.search('id=(.*?)$', comm_url).group(1)
-                c.co_address = '//div[@class="Detailsleft"]/ul[3]/li[2]/text()'
-                c.co_develops = '//div[@class="Detailsleft"]/ul[10]/li[2]/text()'
-                c.co_pre_sale = '//div[@class="Detailsleft"]/ul[12]/li[2]/text()'
-                c.co_name = '//*[@id="divbody"]/div[5]/ul[1]/li[1]/text()'
-                data_list = c.to_dict()
-                p = ProducerListUrl(list_page_url=[comm_url], request_type='get', encode='utf-8',
-                                    analyzer_rules_dict=data_list, analyzer_type='xpath',
-                                    current_url_rule='//div[@class="houbanner"]/a[4]/@href')
-                build_url_list = p.get_details()
-                for i in build_url_list:
-                    b = Building('12')
-                    url = 'http://www.gafdc.cn/newhouse/' + i
-                    b.bu_all_house = '//*[@class="font12black-center"]/td[2]/text()'
-                    b.co_id = c.co_id
-                    b.bu_id = '//*[@class="font12black-center"]/td[5]/text()'
-                    b.bu_pre_sale = '//*[@class="font12black-center"]/td[1]/text()'
-                    p = ProducerListUrl(list_page_url=[comm_url], request_type='get', encode='utf-8',
-                                        analyzer_rules_dict=data_list, analyzer_type='xpath',
-                                        current_url_rule='//*[@class="font12black-center"]/td[6]/a/@onclick')
-                    house_url_list = p.get_details()
-                    for i in house_url_list:
-                        itemRecord = re.search("\('(.*?)'", i).group(1)
-                        houseCode = re.search("'(.*?)'\)", i).group(1)
-                        # floor = self.get_floor(itemRecord, houseCode)
-                        floor = self.get_house_info(itemRecord, houseCode)
+            ho_name_list = re.findall("border-333333'>(.*?)<", html)
+            if not ho_name_list:
+                continue
+            for i in range(0, len(ho_name_list)):
+                h = House(co_index)
+                h.bu_id = re.findall("onclick=GetData\(.*?,'(.*?)'.*?border-F9D6BA", html)[0]
+                h.ho_name = i
+                h.ho_type = re.findall('物业类别：(.*?) ', html)[i]
+                h.ho_build_size = re.findall('建筑面积：(.*?) ', html)[i]
+                h.insert_db()
 
-    def get_house_info(self, itemRecord, houseCode):
-        url = 'http://www.gafdc.cn/newhouse/GetBuildTableByAjax.ashx'
-        data = {
-            'itemRecord': itemRecord,
-            'houseCode': houseCode,
-        }
-        response = requests.post(url=url, data=data)
-        html = response.text
-        tree = etree.XML(html)
-        house_info_list = tree.xpath('//td[@class="border-333333"]/text()')
-        house_info_list = tree.xpath('//td[@class="border-333333"]/@Title')
+    def get_build_info(self, all_build_url_list):
+        b = Building(co_index)
+        b.co_id = "onclick=GetData\('(.*?)',"
+        b.bu_id = "onclick=GetData\('.*?','(.*?)'"
+        b.bu_num = "font12yellow-leftA'>.*?</span>套</td><td>.*?</td><td>(.*?)<"
+        b.bu_all_house = "font12yellow-leftA'>(.*?)<"
+        data_list = b.to_dict()
+        p = ProducerListUrl(page_url=all_build_url_list,
+                            request_type='get', encode='utf-8',
+                            analyzer_rules_dict=data_list,
+                            current_url_rule="onclick=GetData\('(.*?)','(.*?)'\)",
+                            analyzer_type='regex',
+                            headers=self.headers)
+        house_url_list = p.get_details()
+        return house_url_list
 
-# def get_floor(self, itemRecord, houseCode):
-#     url = 'http://www.gafdc.cn/newhouse/GetBuildTableByAjax.ashx'
-#     data = {
-#         'itemRecord': itemRecord,
-#         'houseCode': houseCode,
-#     }
-#     response = requests.post(url=url, data=data)
-#     html = response.text
-#     tree = etree.XML(html)
-#     floor = tree.xpath('//td[@class="border-F9D6BA"]/text()')[0].strip()
-#     return floor
+    def get_comm_url(self, all_url_list):
+        p = ProducerListUrl(page_url=all_url_list,
+                            request_type='get', encode='utf-8',
+                            current_url_rule="<a class='anone' href='(.*?)'",
+                            analyzer_rules_dict=None,
+                            analyzer_type='regex',
+                            headers=self.headers)
+        comm_url_list = p.get_current_page_url()
+        return comm_url_list
 
-# result = requests.get(comm_url)
-# html = result.text
-# tree = etree.HTML(html)
-# comm_detail_url = tree.xpath('//div[@class="houbanner"]/a[2]/@href')[0]
-# url = 'http://www.gafdc.cn/newhouse/' + comm_detail_url
-# response = requests.get(url=url)
-# html = response.text
-# tree = etree.HTML(html)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for i in comm_url_list:
-#     url = 'http://www.gafdc.cn/newhouse/'+i
-#     p = ProducerListUrl(list_page_url=list_page_url, request_type='get', encode='utf-8',
-#                         analyzer_rules_dict=None, analyzer_type='xpath',
-#                         current_url_rule='//div[@class="houbanner"]/a[2]/@href')
-#     html = do_request(url, 'get', None, 'utf-8')
-#     comm_detail = p.get_list_page_url(html)[0]
-#     comm_url = 'http://www.gafdc.cn/newhouse/' + comm_detail
-#     c = Comm('12')
-#     c.co_address = '//div[@class="Detailsleft"]/ul[3]/li[2]/text()'
-#     c.co_develops = '//div[@class="Detailsleft"]/ul[10]/li[2]/text()'
-#     data_t = c.data_type
-#     data_list = c.to_dict()
-#     pass
+    def get_comm_info(self, all_url_list):
+        c = Comm(co_index)
+        c.co_name = "class='newtopleft font-k'>(.*?)</li>"
+        c.co_id = 'form1" method="post" action="house_base\.aspx\?id=(.*?)"'
+        c.co_address = "项目位置：</li><li class='DetaimidR font-f'>(.*?)</li></ul>"
+        c.co_develops = "开发商：</li><li class='DetaimidR font-f'>(.*?)</li>"
+        c.co_volumetric = "容积率：</li><li class='DetaimidR font-f'>(.*?)<"
+        c.co_green = "绿化率：</li><li class='DetaimidR font-f'>(.*?)<"
+        data_list = c.to_dict()
+        p = ProducerListUrl(page_url=all_url_list,
+                            request_type='get', encode='utf-8',
+                            analyzer_rules_dict=data_list,
+                            analyzer_type='regex',
+                            headers=self.headers)
+        p.get_details()
