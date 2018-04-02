3b51eb050076ad8da6292fb49caa0c9ae908fd8f
chengjiwen <chengjiwen@fangjia.com>
2018-04-02 13:18:21 +0800

    hechi

--- crawler2/hechi_16.py
+++ crawler2/hechi_16.py
@@ -1,46 +1,120 @@
-from crawler_base import Crawler
-import requests
-import re
-from tool import Tool
-from lxml import etree
 
+"""
+url = http://www.hcsfcglj.com/Templets/BoZhou/aspx/spflist.aspx
+city : 河池
+CO_INDEX : 16
+author: 程纪文
+"""
+
+
+from crawler_base import Crawler
+from comm_info import Comm, Building, House
+from get_page_num import AllListUrl
+from producer import ProducerListUrl
+import re, requests
+from lxml import etree
+import json
+
+co_index = '16'
+city = '河池'
 
 class Hechi(Crawler):
     def __init__(self):
         self.url = 'http://www.hcsfcglj.com/Templets/BoZhou/aspx/spflist.aspx'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
-            'Cookie': 'ASP.NET_SessionId=lllitzrxbio3mh2s1gpmrhmg',
+            # 'Cookie': 'ASP.NET_SessionId=lllitzrxbio3mh2s1gpmrhmg',
             'Referer': 'http://www.hcsfcglj.com/Templets/BoZhou/aspx/spflist.aspx'
         }
 
     def start_crawler(self):
-        view_info = Tool.get_view_state(url=self.url,
-                                        view_state='//*[@id="__VIEWSTATE"]/@value',
-                                        event_validation='//*[@id="__EVENTVALIDATION"]/@value')
-        page_count = self.get_page_count()
-        self.get_all_detail_url(page_count, view_info)
+        b = AllListUrl(first_page_url=self.url,
+                       request_method='get',
+                       analyzer_type='regex',
+                       encode='utf-8',
+                       page_count_rule='共.*?Count">(\d+)</span>页',
+                       )
+        page = b.get_page_count()
+        formdata = {}
+        for i in range(1,int(page)+1):
+            formdata["__EVENTTARGET"] = "navigate$LnkBtnGoto"
+            formdata["navigate$txtNewPageIndex"] = i
+            try:
+                res = requests.post(self.url,headers=self.headers)
+            except Exception as e:
+                print("co_index={},第{}页翻页失败".format(co_index,i))
+                print(e)
+                continue
+            con = etree.HTML(res.text)
+            formdata["__VIEWSTATE"] = con.xpath("//input[@id='__VIEWSTATE']/@value")[0]
+            formdata["__EVENTVALIDATION"] = con.xpath("//input[@id='__EVENTVALIDATION']/@value")[0]
 
-    def get_all_detail_url(self, page_count, view_info):
-        url_list = []
+            bu_url_list = con.xpath("//td[@style='width:13%']/a/@href")
+            bu_pre = con.xpath("//td[@style='width:13%']/a/text()")
+            bu_dev = con.xpath("//td[@style='width:24%']/text()")
+            co_name = con.xpath("//td[@style='width:15%']/text()")
+            # print(i)
+            for index in range(len(bu_url_list)):
+                bu_detail = "http://www.hcsfcglj.com/Templets/BoZhou/aspx/" + bu_url_list[index]
+                bu_pre_sale = bu_pre[index]
+                bo_develops = bu_dev[index]
+                bu_co_name = co_name[index]
+                try:
+                    bu_res = requests.get(bu_detail,headers=self.headers)
+                except Exception as e:
+                    print("co_index={},楼栋{}无法访问".format(co_index,bu_detail))
+                    print(e)
+                    continue
+                bu_con = bu_res.text
 
-        for i in range(1, page_count + 1):
-            post_data = {'navigate$txtNewPageIndex': str(i),
-                         '__VIEWSTATE': view_info['__VIEWSTATE'],
-                         '__EVENTVALIDATION': view_info['__EVENTVALIDATION'],
-                         '__EVENTTARGET': 'navigate$LnkBtnNext',
-                         '__EVENTARGUMENT': None,
-                         'txt_developer': None,
-                         'txt_code': None,
-                         'txt_address': None,
-                         'txt_Name': None}
-            res = requests.post(url=self.url, data=post_data, headers=self.headers)
+                self.get_build_info(bu_pre_sale,bo_develops,bu_co_name,bu_con)
+                self.get_house_info(bu_con)
 
-            print(res.content.decode())
-        return url_list
 
-    def get_page_count(self):
-        res = requests.get(self.url)
-        html_str = res.content.decode()
-        count = re.search('navigate_LblPageCount">(.*?)</span>', html_str, re.S | re.M).group(1)
-        return int(count)
+
+    def get_build_info(self,bu_pre_sale,bo_develops,bu_co_name,bu_con):
+
+        build = Building(co_index)
+
+        build.bu_id = re.search('编号.*?>(\d+)<',bu_con,re.S|re.M).group(1)
+        build.bu_num = re.search('幢号.*?>(\d+)<',bu_con,re.S|re.M).group(1)
+        build.bu_floor = re.search('总层数.*?>(\d+)<',bu_con,re.S|re.M).group(1)
+        build.bu_build_size = re.search('预售建筑面积.*?>(\d+.\d+)<',bu_con,re.S|re.M).group(1)
+        build.bu_address = re.search('楼房坐落.*?;">(.*?)</span',bu_con,re.S|re.M).group(1)
+        build.bu_live_size = re.search('住宅建筑面积.*?>(\d+.\d+)<',bu_con,re.S|re.M).group(1)
+        build.bu_not_live_size = re.search('非住宅建筑面积.*?;">(.*?)</span',bu_con,re.S|re.M).group(1)
+        build.bo_build_start_time = re.search('开工日期.*?;">(.*?)</span',bu_con,re.S|re.M).group(1)
+        build.bu_all_house = re.search('总套数.*?>(\d+)<',bu_con,re.S|re.M).group(1)
+        build.bu_pre_sale = bu_pre_sale
+        build.bo_develops = bo_develops
+        build.co_name = bu_co_name
+        build.insert_db()
+
+    def get_house_info(self,bu_con):
+        bu_html = etree.HTML(bu_con)
+        house = House(co_index)
+        ho = bu_html.xpath("//tr[@height='30']//span/a")
+        bu_id = re.search('编号.*?>(\d+)<',bu_con,re.S|re.M).group(1)
+        for ho_info in ho:
+            try:
+                ho_detail = "http://www.hcsfcglj.com/Templets/BoZhou/aspx/" + ho_info.xpath("./@value")[0]
+                try:
+                    ho_res = requests.get(ho_detail,headers=self.headers)
+                    ho_con = ho_res.text
+                except Exception as e:
+                    print("co_index={},房屋详情页{}请求失败".format(co_index,ho_detail))
+                    print(e)
+                    continue
+                house.ho_name = re.search('房号.*?<td>(.*?)<',ho_con,re.S|re.M).group(1)
+                house.ho_floor = re.search('所在层.*?<td>(.*?)<',ho_con,re.S|re.M).group(1)
+                house.ho_share_size = re.search('分摊共有面积.*?<td>(.*?)<',ho_con,re.S|re.M).group(1)
+                house.ho_build_size = re.search('建筑面积.*?<td>(.*?)<',ho_con,re.S|re.M).group(1)
+                house.ho_true_size = re.search('套内面积.*?<td>(.*?)<',ho_con,re.S|re.M).group(1)
+                house.ho_type = re.search('房屋用途.*?<td>(.*?)<',ho_con,re.S|re.M).group(1)
+                house.bu_num = re.search('幢号.*?<td>(.*?)<',ho_con,re.S|re.M).group(1)
+                house.bu_id = bu_id
+            except:
+                house.ho_name = ho_info.xpath("./@id")[0]
+                house.bu_id = bu_id
+
+            house.insert_db()
