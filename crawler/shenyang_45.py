0d6bb78fcdefb5985574b82573358bc8b19cb51a
chengjiwen <chengjiwen@fangjia.com>
2018-04-09 14:46:21 +0800

    'shenyang'

--- crawler/shenyang_45.py
+++ crawler/shenyang_45.py
@@ -16,6 +16,7 @@ from multiprocessing import Process, Queue
 
 co_index = 45
 count = 0
+city = 'shenyang'
 
 
 class Shenyang(Crawler):
@@ -33,18 +34,18 @@ class Shenyang(Crawler):
             try:
                 res = requests.get(url, headers=self.headers)
             except Exception as e:
-                print(e)
+                print("co_index={},翻页问题".format(co_index),e)
                 continue
             con_html = etree.HTML(res.text)
             html = con_html.xpath("//table[@bgcolor='#a4abb5']//tr")[1:-1]
             for tag in html:
+                build_all_url, co_id = self.comm(tag)
                 try:
-                    build_all_url, co_id = self.comm(tag)
                     res = requests.get(build_all_url,headers=self.headers)
-
                     self.build(res,co_id)
-                except:
-                    continue
+                except Exception as e:
+                    print("co_index={},小区详情页无法访问".format(co_index),e)
+
 
     def comm(self, tag):
         co = Comm(co_index)
@@ -53,7 +54,7 @@ class Shenyang(Crawler):
         co.co_develops = tag.xpath("./td[@width='192']/text()")[0]
         co_id = tag.xpath("./td/a/@href")[0]
         co.co_id = re.search('mmcid=(\d+)&', co_id).group(1)
-
+        co.co_open_time = tag.xpath("./td[@width='192']/text()")[0]
         buid_all_url = "http://www.syfc.com.cn" + co_id
         co.insert_db()
         global count
@@ -73,29 +74,38 @@ class Shenyang(Crawler):
                 bu.bu_id = re.search('houseid=(\d+)&', house_url).group(1)
 
                 bu.insert_db()
-            except:
+            except Exception as e:
+                print("co_index={},楼栋信息错误".format(co_index),e)
                 continue
+
             self.house(house_url, bu.bu_id, co_id)
 
     def house(self, house_url, bu_id, co_id):
-        ho = House(co_index)
+
         ho_url = "http://www.syfc.com.cn" + house_url
-        res = requests.get(ho_url, headers=self.headers)
-        con = etree.HTML(res.text)
-        ho_detail_url = con.xpath("//iframe/@src")[0]
-        response = requests.get(ho_detail_url, headers=self.headers)
+        try:
+            res = requests.get(ho_url, headers=self.headers)
+            con = etree.HTML(res.text)
+            ho_detail_url = con.xpath("//iframe/@src")[0]
+            response = requests.get(ho_detail_url, headers=self.headers)
+        except Exception as e:
+            print("co_index={},楼栋详情页无法访问".format(co_index),e)
         html = etree.HTML(response.text)
         content = html.xpath("//td[@width='70']")
         for td in content:
+            ho = House(co_index)
             try:
                 room_url = td.xpath("./a/@href")[0]
                 ho.ho_name = td.xpath("./a/text()")[0]
-                ho.ho_id = re.search('id=(\d+)&', room_url).group(1)
+                # ho.ho_id = re.search('id=(\d+)&', room_url).group(1)
                 ho.bu_id = bu_id
                 ho.co_id = co_id
                 room_url = "http://www.syfc.com.cn" + room_url
-                res = requests.get(room_url, headers=self.headers)
-                con = res.text
+                try:
+                    res = requests.get(room_url, headers=self.headers)
+                    con = res.text
+                except Exception as e:
+                    print("co_idnex={},房屋详情页无法访问".format(co_index),e)
                 # print(con)
                 ho.ho_build_size = re.search('建筑面积.*?">(.*?)<', con, re.S | re.M).group(1)
                 ho.ho_share_size = re.search('分摊面积.*?">(.*?)<', con, re.S | re.M).group(1)
