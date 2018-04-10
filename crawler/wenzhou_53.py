97fd9d21f889b49e234a3f1bf455ac9a84d487f8
e <lvsanli@fangjia.com>
2018-04-10 13:48:20 +0800

    53 40 54 55

--- crawler/wenzhou_53.py
+++ crawler/wenzhou_53.py
@@ -24,16 +24,19 @@ class Wenzhou(object):
     def start_crawler(self):
         for i in range(119):
             all_page_url = url + '?currPage=' + str(i)
-            response = requests.get(all_page_url, headers=self.headers)
-            html = response.text
-            comm_url_list = re.findall("(FirstHandProjectInfo.*?)'", html, re.S | re.M)
-            self.get_comm_info(comm_url_list)
+            try:
+                response = requests.get(all_page_url, headers=self.headers)
+                html = response.text
+                comm_url_list = re.findall("(FirstHandProjectInfo.*?)'", html, re.S | re.M)
+                self.get_comm_info(comm_url_list)
+            except Exception as e:
+                print('page页错误，co_index={},url={},page={}'.format(co_index, all_page_url, i), e)
 
     def get_comm_info(self, comm_url_list):
         for i in comm_url_list:
+            comm_url = 'http://www.wzfg.com/realweb/stat/' + i
             try:
                 comm = Comm(co_index)
-                comm_url = 'http://www.wzfg.com/realweb/stat/' + i
                 response = requests.get(comm_url, headers=self.headers)
                 html = response.text
                 comm.co_develops = re.search('开发单位：.*?<a.*?>(.*?)<', html, re.S | re.M).group(1)
@@ -49,17 +52,17 @@ class Wenzhou(object):
                 comm.insert_db()
                 build_info_list = re.findall("floortitle' title='(.*?)'", html, re.S | re.M)
                 bu_num_list = re.findall("floortitle.*?>(.*?)<", html, re.S | re.M)
-                self.get_build_info(build_info_list, bu_num_list, comm.co_id)
+                self.get_build_info(build_info_list, bu_num_list, comm.co_id, comm_url)
             except Exception as e:
-                print(e)
+                print('小区错误，co_index={},url={}'.format(co_index, comm_url), e)
 
-    def get_build_info(self, build_info_list, bu_num_list, co_id):
+    def get_build_info(self, build_info_list, bu_num_list, co_id, comm_url):
         for i in range(len(bu_num_list)):
             try:
                 build = Building(co_index)
                 build.co_id = co_id
-                build.build_info = build_info_list[i]
+                build.info = build_info_list[i]
                 build.bu_num = bu_num_list[i]
                 build.insert_db()
             except Exception as e:
-                print(e)
+                print('楼栋错误，co_index={},url={}'.format(co_index, comm_url), e)
--- crawler/xiamen_40.py
+++ crawler/xiamen_40.py
@@ -22,40 +22,30 @@ class Xiamen(Crawler):
         }
 
     def start_crawler(self):
-        co = Comm(co_index)
         for i in range(1, 10000):
+            formdata = {
+                "currentpage": i,
+                "pagesize": 20,
+            }
             try:
-                formdata = {
-                    "currentpage": i,
-                    "pagesize": 20,
-                }
                 res = requests.post("http://fdc.xmtfj.gov.cn:8001/home/Getzslp", data=formdata, headers=self.headers)
                 con = json.loads(res.text)
-                con = str(con)
-                co_name = re.findall('"XMMC":"(.*?)"', con)
-                co_id = re.findall('"TRANSACTION_ID":"(.*?)"', con)
-                co_address = re.findall('"XMDZ":"(.*?)"', con)
-                co_pre_sale = re.findall('"YSXKZH":"(\d+)"', con)
-                co_all_house = re.findall('"PZTS":(\d+)', con)
-                co_build_size = re.findall('"PZMJ":(\d+)', con)
-                co_area = re.findall('"XMDQ":"(.*?)"', con)
-                co_pre_date = re.findall('"GETDATE":"(.*?)"', con)
-                if len(co_name) == 0:
-                    break
-                else:
-                    for index in range(1, len(co_name) + 1):
-                        co.co_name = co_name[index]
-                        co.co_id = co_id[index]
-                        co.co_address = co_address[index]
-                        co.co_pre_sale_date = co_pre_date[index]
-                        co.co_pre_sale = co_pre_sale[index]
-                        co.area = co_area[index]
-                        co.co_build_size = co_build_size[index]
-                        co.co_all_house = co_all_house[index]
-
-                        co.insert_db()
-            except:
-                continue
+                body = con['Body']
+                info_dict = json.loads(body)['bodylist']
+                for i in info_dict:
+                    comm = Comm(co_index)
+                    comm.co_name = i['XMMC']
+                    comm.co_id = i['TRANSACTION_ID']
+                    comm.co_address = i['XMDZ']
+                    comm.co_pre_sale = i['YSXKZH']
+                    comm.co_all_house = i['PZTS']
+                    comm.co_build_size = i['PZMJ']
+                    comm.co_area = i['XMDQ']
+                    comm.co_pre_date = i['GETDATE']
+                    comm.insert_db()
+            except Exception as e:
+                print('小区错误，co_index={},url={},data'.format(co_index, 'http://fdc.xmtfj.gov.cn:8001/home/Getzslp',
+                                                            formdata), e)
 
 
 if __name__ == '__main__':
