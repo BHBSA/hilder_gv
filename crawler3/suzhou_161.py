ee315a0fcdb008c70f4315c75208fda6a50eab69
chengjiwen <chengjiwen@fangjia.com>
2018-06-21 16:18:33 +0800

    suhzou

--- crawler3/suzhou_161.py
+++ crawler3/suzhou_161.py
@@ -19,8 +19,93 @@ log = LogHandler('苏州')
 
 class Suzhou(Crawler):
     def __init__(self):
-        self.start_url = '3'
+        self.start_url = 'http://spf.szfcweb.com/szfcweb/(S(hdeijxnxs01rlpun22pnfha0))/DataSerach/SaleInfoProListIndex.aspx'
         self.headers = {
             'User-Agent':
                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119Safari/537.36',
         }
+
+    def start_crawler(self):
+        res = requests.get(self.start_url)
+        html = etree.HTML(res.content.decode())
+        viewstate = html.xpath('//input[@id="__VIEWSTATE"]/@value')[0]
+        generator = html.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')[0]
+        validation = html.xpath('//input[@id="__EVENTVALIDATION"]/@value')[0]
+        select = '查询'
+        for region in ['工业园区','吴中区','高新区','相城区','姑苏区','吴江区']:
+            rd_code = region
+            formdata = {'__VIEWSTATE':viewstate,
+                        '__VIEWSTATEGENERATOR':generator,
+                        '__EVENTVALIDATION':validation,
+                        'ctl00$MainContent$ddl_RD_CODE':rd_code,
+                        'ctl00$MainContent$bt_select':select}
+            index_res = requests.post(self.start_url,data=formdata)
+            page = re.search('nbsp共&nbsp(\d+)&nbsp页',index_res.text).group(1)
+            data = {}
+            for i in range(1,int(page)+1):
+                if i==1:
+                    index_html = etree.HTML(index_res.text)
+                    view_state = index_html.xpath('//input[@id="__VIEWSTATE"]/@value')[0]
+                    generator_ = index_html.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')[0]
+                    validation_ = index_html.xpath('//input[@id="__EVENTVALIDATION"]/@value')[0]
+                    data = {'__VIEWSTATE':view_state,
+                        '__VIEWSTATEGENERATOR':generator_,
+                        '__EVENTVALIDATION':validation_,
+                        'ctl00$MainContent$ddl_RD_CODE':rd_code,
+                        '__EVENTTARGET':'ctl00$MainContent$OraclePager1$ctl12$PageList',
+                        'ctl00$MainContent$OraclePager1$ctl12$PageList':i-1,}
+                else:
+                    page_res = requests.post(self.start_url,data=data)
+                    page_html = etree.HTML(page_res.text)
+                    view_state = page_html.xpath('//input[@id="__VIEWSTATE"]/@value')[0]
+                    generator_ = page_html.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')[0]
+                    validation_ = page_html.xpath('//input[@id="__EVENTVALIDATION"]/@value')[0]
+                    data = {'__VIEWSTATE': view_state,
+                            '__VIEWSTATEGENERATOR': generator_,
+                            '__EVENTVALIDATION': validation_,
+                            'ctl00$MainContent$ddl_RD_CODE': rd_code,
+                            '__EVENTTARGET': 'ctl00$MainContent$OraclePager1$ctl12$PageList',
+                            'ctl00$MainContent$OraclePager1$ctl12$PageList': i - 1, }
+                    url_list = page_html.xpath('//td/a/@href')
+                    for url in url_list[:9]:
+                        co_url = 'http://spf.szfcweb.com/szfcweb/(S(hdeijxnxs01rlpun22pnfha0))/DataSerach/'+url
+                        co_res = requests.get(co_url,headers=self.headers)
+                        co = Comm(co_index)
+                        co.co_name = re.search('项目名称.*?NAME">(.*?)</span',co_res.text).group(1)
+                        co.co_id = re.search('ID=(.*)',url).group(1)
+                        co.co_develops = re.search('公司名称.*?Com">(.*?)</span',co_res.text).group(1)
+                        co.co_address = re.search('项目地址.*?Add">(.*?)</span',co_res.text).group(1)
+                        co.insert_db()
+                        bu_list = page_html.xpath("//table[@id='ctl00_MainContent_OraclePager1']/tbody/tr")
+                        self.bu_parse(co.co_id,bu_list)
+
+    def bu_parse(self,co_id,bu_list):
+        for bu in bu_list[1:]:
+            build = Building(co_index)
+            build.co_id = co_id
+            build.bu_id = bu.xpath("//form/@action")[0]
+            build.bu_num = bu.xpath("./td/a/text()")[0]
+            build.bu_build_size = bu.xpath("./td[1]/text()")[0]
+            build.bu_floor = bu.xpath("./td[3]/text()")[0]
+            build.bu_all_house = bu.xpath("./td[2]/text()")[0]
+            build.insert_db()
+            tmp_url = bu.xpath("./td/a/@href")[0]
+            house_url = 'http://spf.szfcweb.com/szfcweb/(S(hdeijxnxs01rlpun22pnfha0))/DataSerach/'+tmp_url
+            self.ho_parse(co_id,build.bu_id,house_url)
+
+    def ho_parse(self,co_id,bu_id,house_url):
+        res = requests.get(house_url,headers=self.headers)
+        html = etree.HTML(res.text)
+        ho_list = html.xpath("//td[@align]")
+        for house in ho_list:
+            ho = House(co_index)
+            ho.co_id = co_id
+            ho.bu_id = bu_id
+            ho.ho_name = house.xpath("./text()")[0]
+            ho.insert_db()
+
+
+
+
+
+
